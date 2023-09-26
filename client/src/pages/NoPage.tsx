2be042801d50956c5f779befbf2736b9400fd400
Xenko10 <kubinios_2002@o2.pl>
2023-09-26 21:16:29 +0200

    style nopage

--- client/src/pages/NoPage.tsx
+++ client/src/pages/NoPage.tsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom";
+import styles from "./css/NoPage.module.css";
+
 export default function NoPage() {
-  return <h1>NoPage</h1>;
+  return (
+    <div className={styles.main}>
+      <p className={styles.mainParagraph}>
+        We can't seem to find the page you're looking for.
+      </p>
+      <Link to='/' className={styles.mainGoToHome}>
+        Go to homepage
+      </Link>
+    </div>
+  );
 }
