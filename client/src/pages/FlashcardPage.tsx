6e3909fdff4141bd667d242538c6033e4887a1f5
Xenko10 <kubinios_2002@o2.pl>
2023-09-28 12:34:54 +0200

    add change answer/question on click, add buttons to change number on question

--- client/src/pages/FlashcardPage.tsx
+++ client/src/pages/FlashcardPage.tsx
@@ -1,9 +1,35 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 import axios from "axios";
+import styles from "./css/FlashcardPage.module.css";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+interface Action {
+  type: string;
+  payload?: { amount: number };
+}
+
+const ACTIONS = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+};
+
+function reducer(count: number, action: Action) {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return count + 1;
+    case ACTIONS.DECREMENT:
+      return count - 1;
+    default:
+      return count;
+  }
+}
 
 export default function FlashcardPage() {
-  const [qnaList, setQnaList] = useState([]);
+  const [count, dispatch] = useReducer(reducer, 0);
+  const [qnaList, setQnaList] = useState([{ question: "", answer: "" }]);
+  const [isQuestion, setIsQuestion] = useState(true);
   const { flashcardId } = useParams();
 
   useEffect(() => {
@@ -22,5 +48,40 @@ export default function FlashcardPage() {
     console.log(qnaList);
   }, [qnaList]);
 
-  return <div>asd</div>;
+  return (
+    <div className={styles.main}>
+      <ArrowBackIosIcon
+        color={count === 0 ? "disabled" : undefined}
+        onClick={
+          count > 0
+            ? () => {
+                dispatch({ type: ACTIONS.DECREMENT });
+                setIsQuestion(true);
+              }
+            : () => {}
+        }
+      />
+      <div
+        onClick={() => {
+          setIsQuestion(!isQuestion);
+        }}>
+        {qnaList.length > count
+          ? isQuestion
+            ? qnaList[count].question
+            : qnaList[count].answer
+          : ""}
+      </div>
+      <ArrowForwardIosIcon
+        color={count === qnaList.length - 1 ? "disabled" : undefined}
+        onClick={
+          count < qnaList.length - 1
+            ? () => {
+                dispatch({ type: ACTIONS.INCREMENT });
+                setIsQuestion(true);
+              }
+            : () => {}
+        }
+      />
+    </div>
+  );
 }
