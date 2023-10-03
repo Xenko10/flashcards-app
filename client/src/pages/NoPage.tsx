c9472133e4f31dce3f9c140aaf4e4b97a1f3a84e
Xenko10 <kubinios_2002@o2.pl>
2023-10-03 13:57:08 +0200

    change nopage

--- client/src/pages/NoPage.tsx
+++ client/src/pages/NoPage.tsx
@@ -4,9 +4,9 @@ import styles from "./css/NoPage.module.css";
 export default function NoPage() {
   return (
     <div className={styles.main}>
-      <p className={styles.mainParagraph}>
+      <h1 className={styles.mainH1}>
         We can't seem to find the page you're looking for.
-      </p>
+      </h1>
       <Link to='/' className={styles.mainGoToHome}>
         Go to homepage
       </Link>
